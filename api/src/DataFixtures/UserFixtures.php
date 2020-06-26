057cfb3d1d805ef9288fe548102faf8f411f26e9
Houziaux mike / Jenaye <jenaye@protonmail.com>
2020-06-26 13:13:29 +0200

    add route

--- api/src/DataFixtures/UserFixtures.php
+++ api/src/DataFixtures/UserFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
